8bb64ae824d1b927a636ec20876a7b242c9c4d8f
JDgomez2002 <87883400+JDgomez2002@users.noreply.github.com>
2025-04-02 19:39:16 -0600

    feat: auth middleware

--- api/src/features/storage/index.ts
+++ api/src/features/storage/index.ts
@@ -1,11 +1,22 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { uploadFileSchema } from "./schemas";
+import { jwt } from 'hono/jwt'
+import type { JwtVariables } from 'hono/jwt'
 
-const app = new Hono();
+const JWT_SECRET_KEY = process.env.JWT_KEY!
+
+type Variables = JwtVariables
+
+const app = new Hono<{ Variables: Variables }>();
+
+app.use(
+  "*",
+  jwt({ secret: JWT_SECRET_KEY })
+);
 
 app.get("/", async (c) => {
-  return c.json({ message: "List files endpoint" });
+  return c.json({ success: true, message: "List files endpoint" });
 });
 
 app.post("/upload", zValidator("json", uploadFileSchema), async (c) => {
